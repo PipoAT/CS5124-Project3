2b64fd393b5eb6018df227201557e5fe4a9605fd
Owen Edwards <edwardoa@mail.uc.edu>
2025-04-24 17:55:22 -0400

    Updated charactersArray

--- js/data_structures/charactersArray.js
+++ js/data_structures/charactersArray.js
@@ -1,5 +1,28 @@
 // data_structures/charactersArray.js
-const charactersArray = ["Walter", "Jesse", "Skyler", "Hank", "Saul", "Gus", "Mike", "Marie", "Todd"];
 
-export default  charactersArray ;
-  
+const charactersArray = [
+  "Walter", // Walter White
+  "Jesse", // Jesse Pinkman
+  "Skyler", // Skyler White
+  "Hank", // Hank Schrader
+  "Saul", // Saul Goodman
+  "Gus", // Gus Fring
+  "Mike", // Mike Ehrmantraut
+  "Marie", // Marie Schrader
+  "Todd", // Todd Alquist
+  "Walt Jr.", // Walter White, Jr.
+  "Skinny Pete",
+  "Badger", // Brandon Mayhew
+  "Jane", // Jane Margolis
+  "Ted", // Ted Beneke
+  "Lydia", // Lydia Rodarte-Quayle
+  "Gomez", // Steven Gomez
+  "Huell", // Huell Babineaux
+  "Tyrus", // Tyrus Kitt
+  "Carmen", // Carmen Molina
+  "Andrea", // Andrea Cantillo
+  "Francesca", // Francesca Liddy
+  "Tio", // Hector Salamanca
+];
+
+export default charactersArray;
