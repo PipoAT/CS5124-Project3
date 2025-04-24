43fd4138899cc637b6392aaedbd14ad67c376601
Owen Edwards <edwardoa@mail.uc.edu>
2025-04-24 17:01:30 -0400

    Added backbone to visualizations

--- js/barChart.js
+++ js/barChart.js
@@ -1,6 +1,3 @@
-const svg = d3.select("#barChart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+function renderCharacterBarChart() {
+
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+import { renderCharacterBarChart } from './barChart.js';
+import { renderCharacterLineChart } from './lineChart.js';
+import { renderCharacterWordCloud } from './wordCloud.js';
+import { renderShowPieChart } from './showPieChart.js';
+import { renderShowArcDiagram } from './showArcDiagram.js';
+
 function loadData() {
  // TODO: Load data from a TXT file and parse it into a usable format, e.g. JS sets or arrays
 }
