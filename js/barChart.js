ca75b6e4b4e6d8a3d93b320d05b3bc773ae9fdcc
Owen Edwards <edwardoa@mail.uc.edu>
2025-04-24 16:55:21 -0400

    Added visualizations

---
+++ js/barChart.js
@@ -0,0 +1,6 @@
+const svg = d3.select("#barChart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
